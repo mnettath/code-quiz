49790518bbc00a5a6a9fa4a6fd8048fcc9b84376
Maya N <nettathm@gmail.com>
2023-08-29 14:34:47 -0600

    updated code in index.html and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,115 @@
-// VARIABLES
+//create references to to the HTML elements
+var startButton = document.getElementById("start-btn");
+var timerDisplay = document.getElementById("timerDisplay");
+var questionElement = document.getElementById("question");
+var choicesElement = document.getElementById("choices");
+var submitButton = document.getElementById("submit");
+var startSection = document.getElementById("start");
+var quizSection = document.getElementById("quiz");
+var resultSection = document.getElementById("result");
+var initials = document.getElementById("initials");
+//define variables and objects for questions
+//array of objects
 var questions = [
   {
-    title: "What is your favorite color?",
-    choices: ["red", "green", "blue"],
-    answer: ["red"],
+    question: "question 1",
+    choices: ["words", "words", "words", "words"],
+    correctAnswer: 0,
   },
   {
-    title: "What is your favorite animal?",
-    choices: ["tiger", "monkey", "snake"],
-    answer: ["tiger"],
+    question: "question 2",
+    choices: ["words", "words", "words", "words"],
+    correctAnswer: 3,
   },
   {
-    title: "What is your favorite flower?",
-    choices: ["rose", "sunflower", "tulip"],
-    answer: ["rose"],
+    question: "question 3",
+    choices: ["words", "words", "words", "words"],
+    correctAnswer: 1,
+  },
+  {
+    question: "question 4",
+    choices: ["words", "words", "words", "words"],
+    correctAnswer: 4,
+  },
+  {
+    question: "question 5",
+    choices: ["words", "words", "words", "words"],
+    correctAnswer: 2,
   },
 ];
-
 var currentQuestionIndex = 0;
+var currentQ;
+// START SECTION
+var timeScore = 100;
+var timeInterval;
+//start timer/quiz
+function startQuiz() {
+  startSection.classList.add("hide");
+  quizSection.classList.remove("hide");
+  timeInterval = setInterval(function () {
+    timeScore--;
+    timerDisplay.textContent = timeScore + " seconds remaining";
 
-// i want to create a variable for question1
-var question1 = document.querySelector("#question1");
-console.log(question1);
-
-var question2 = document.querySelector("#question2");
-console.log(question2);
-
-var question3 = document.querySelector("#question3");
-
-var start = document.querySelector("#start");
-
-var quiz = document.querySelector("#quiz");
-
-//FUNCTIONS
-
-function hideQuiz2() {
-  quiz.style.visibility = "hidden";
+    if (timeScore === 0) {
+      timerDisplay.textContent = timeScore;
+      gameOver();
+    }
+  }, 1000);
+  displayQuestion();
 }
-hideQuiz2();
+//create a function that displays time
+function displayMessage() {
+  var timeUp = 0;
+  var timeInt = setInterval(function () {
+    if (time < 0) {
+      clearInterval(timeInt);
+    } else {
+      timerDisplay.textContent = time;
+    }
+  });
+}
+//QUIZ SECTION
+//create a function to display the current question and choices
+function displayQuestion() {
+  currentQ = questions[currentQuestionIndex];
+  questionElement.textContent = currentQ.question;
+  choicesElement.textContent = " ";
+  for (let i = 0; i < currentQ.choices.length; i++) {
+    const choice = currentQ.choices[i];
+    var li = document.createElement("li");
+    li.setAttribute("id", i);
+    li.textContent = choice;
+    li.addEventListener("click", answerSel);
+    choicesElement.append(li);
+  }
+}
+function answerSel(event) {
+  var userChoice = event.target.id;
+  console.log(userChoice);
+  //check if answer is correct -- > check user choice with correct (if statement)
+  if (userChoice != currentQ.correctAnswer) {
+    timeScore -= 15;
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length && timeScore > 0) {
+    displayQuestion();
+  } else {
+    gameOver();
+  }
+}
+function gameOver() {
+  quizSection.classList.add("hide");
+  resultSection.classList.remove("hide");
+  clearInterval(timeInterval);
+}
 
-start.addEventListener("click", function () {
-  start.style.visibility = "hidden";
-  question1.style.visibility = "visible";
-});
-
-// for loop?
-question1.addEventListener("click", function () {
-  question1.style.visibility = "hidden";
-  question2.style.visibility = "visible";
-});
-
-question2.addEventListener("click", function () {
-  question2.style.visibility = "hidden";
-  question3.style.visibility = "visible";
-});
+//store score information in results page and sort them in descending order
+//add event listener for start button
+startButton.addEventListener("click", startQuiz);
+//store score in local storage and present it back into another form -->
+//scores:
+//initials:
+//key value --> high scores --> array of objects that look the same in their key structure
+//loop for rendering each high score
+localStorage.setItem;
+//separate pages for quiz and high scores
